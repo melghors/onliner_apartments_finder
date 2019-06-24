8e1b34f17ea163e28cc59dee83fb97d67ffe4d97
roman.kuchuk <roman.kuchuk@synesis.ru>
2019-06-24 10:54:28 +0300

    Add generate of url

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/robfig/cron"
 	"gopkg.in/telegram-bot-api.v4"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
+	"strings"
 	"time"
 )
 
@@ -36,6 +37,11 @@ func DeleteChannel(c *channels, channelId int64) {
 	delete(*c, channelId)
 }
 
+func generateApiRequest (min *string, max *string, rooms *string) string {
+	url := "https://ak.api.onliner.by/search/apartments?rent_type%5B%5D=" + *rooms + "_rooms&price%5Bmin%5D=" + *min + "&price%5Bmax%5D=" + *max +" &currency=usd&only_owner=true&bounds%5Blb%5D%5Blat%5D=53.709307173772835&bounds%5Blb%5D%5Blong%5D=27.36625671386719&bounds%5Brt%5D%5Blat%5D=54.08638172488552&bounds%5Brt%5D%5Blong%5D=27.75833129882813&v=0.1609207785679565"
+	return url
+}
+
 func initBot () {
 	channels := make(channels)
 	bot, err := tgbotapi.NewBotAPI(loadBotConfig())
@@ -46,13 +52,16 @@ func initBot () {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 	updates, err := bot.GetUpdatesChan(u)
-	var url = "https://ak.api.onliner.by/search/apartments?rent_type%5B%5D=1_room&rent_type%5B%5D=2_rooms&price%5Bmin%5D=300&price%5Bmax%5D=600&currency=usd&only_owner=true&bounds%5Blb%5D%5Blat%5D=53.69914561462634&bounds%5Blb%5D%5Blong%5D=27.36625671386719&bounds%5Brt%5D%5Blat%5D=54.09604689032579&bounds%5Brt%5D%5Blong%5D=27.75833129882813&v=0.18898162215768832"
+	var minPrice = ""
+	var maxPrice = ""
+	var roomsCount = ""
+	var url = "https://ak.api.onliner.by/search/apartments?rent_type%5B%5D=1_room&rent_type%5B%5D=2_rooms&price%5Bmin%5D=400&price%5Bmax%5D=600&currency=usd&only_owner=true&bounds%5Blb%5D%5Blat%5D=53.69914561462634&bounds%5Blb%5D%5Blong%5D=27.36625671386719&bounds%5Brt%5D%5Blat%5D=54.09604689032579&bounds%5Brt%5D%5Blong%5D=27.75833129882813&v=0.18898162215768832"
 	c := Apartment{}
 	oldMap := make(apartmentsIds)
 
 	cr := cron.New()
 	_ = cr.AddFunc("*/1 * * * * *", func() {
-		message, diff := getNewApartments(&url, &c, &oldMap)
+		message, diff := getNewApartments(generateApiRequest(&minPrice, &maxPrice, &roomsCount), &c, &oldMap)
 		if len(diff) != 0 {
 			for channel := range channels {
 				test := tgbotapi.NewMessage(channel, message)
@@ -66,9 +75,9 @@ func initBot () {
 	cr.Start()
 
 	for update := range updates {
-		switch {
-			case update.Message.Text == "/get":
-				message, diff := getNewApartments(&url, &c, &oldMap)
+		switch update.Message.Command(){
+			case "get":
+				message, diff := getNewApartments(generateApiRequest(&minPrice, &maxPrice, &roomsCount), &c, &oldMap)
 				if len(diff) != 0 {
 					for channel := range channels {
 						test := tgbotapi.NewMessage(channel, message)
@@ -85,21 +94,17 @@ func initBot () {
 							panic(err)
 						}
 					}
-		    case update.Message.Text == "/help":
-		    	s := "/get - get list of new aparments in Minsk city \n/channelid - get your channel id \n/start - register \n/exit - unregister"
+		    case "help":
+		    	s := "/start - register \n/exit - unregister \n/set_price_range - example: /set_price_range 200 600 \nset_rooms_count - example: /set_rooms_count 1"
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, s)
 				_, err := bot.Send(msg)
 				if err != nil {
 					panic(err)
 				}
-			case update.Message.Text == "/channelid":
-				s := strconv.FormatInt(update.Message.Chat.ID, 10)
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, s)
-				_, err := bot.Send(msg)
-				if err != nil {
-					panic(err)
-				}
-			case update.Message.Text == "/start":
+				fmt.Println(minPrice)
+				fmt.Println(maxPrice)
+				fmt.Println(roomsCount)
+			case  "start":
 				addChannel(&channels, update.Message.Chat.ID)
 				s := "You are registered for updates!"
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, s)
@@ -107,7 +112,7 @@ func initBot () {
 				if err != nil {
 					panic(err)
 				}
-			case update.Message.Text == "/exit":
+			case "exit":
 				DeleteChannel(&channels, update.Message.Chat.ID)
 				s := "You are unregistered from updates!"
 				msg := tgbotapi.NewMessage(update.Message.Chat.ID, s)
@@ -115,9 +120,41 @@ func initBot () {
 				if err != nil {
 					panic(err)
 				}
+			case "set_price_range":
+				priceRange := update.Message.CommandArguments()
+				s := strings.Split(priceRange, " ")
+				if len(priceRange) == 2 {
+					minPrice, maxPrice = s[0], s[1]
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Price range successfully configured!")
+					_, err = bot.Send(msg)
+					_, err := bot.Send(msg)
+					if err != nil {
+						panic(err)
+					}
+				} else {
+					s := "Set min and max price in USD! For example: /set_price_range 200 600"
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, s)
+					_, err := bot.Send(msg)
+					if err != nil {
+						panic(err)
+					}
+				}
+
+			case "set_rooms_count":
+				rooms := update.Message.CommandArguments()
+				if len(rooms) == 1 {
+					roomsCount = rooms
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Rooms count successfully configured!")
+					_, err = bot.Send(msg)
+					_, err := bot.Send(msg)
+					if err != nil {
+						panic(err)
+					}
+				}
 		}
 	}
 }
+
 
 type apartmentsIds map[int]struct{}
 
@@ -148,12 +185,12 @@ type Apartment struct {
 	} `json:"apartments"`
 }
 
-func getApartments(c *Apartment, url *string) apartmentsIds {
+func getApartments(c *Apartment, url string) apartmentsIds {
 	m := make(apartmentsIds)
 	spaceClient := http.Client{
 		Timeout: time.Second * 2, // Maximum of 2 secs
 	}
-	req, err := http.NewRequest(http.MethodGet, *url, nil)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -176,7 +213,7 @@ func getApartments(c *Apartment, url *string) apartmentsIds {
 	return m
 }
 
-func getNewApartments (url *string, c *Apartment, om *apartmentsIds) (string, []int) {
+func getNewApartments (url string, c *Apartment, om *apartmentsIds) (string, []int) {
 	nm := getApartments(c, url)
 	var diffApartments []int
 	var buffer bytes.Buffer
